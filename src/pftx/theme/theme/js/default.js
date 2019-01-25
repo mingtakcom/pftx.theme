b0f15f7039a4e7fb766a4e13ae376d2039be089c
Andy Fang <andy@mingtak.com.tw>
2019-01-25 23:21:44 +0800

    add theme files

---
+++ src/pftx/theme/theme/js/default.js
@@ -0,0 +1,15 @@
+﻿// JavaScript Document
+$(document).ready(function(){
+	//nav_icon
+	$("#nav_icon").click(function(){
+		if($("nav ul").hasClass( "open" )) $("nav ul").slideUp().removeClass("open");
+		else $("nav ul").slideDown().addClass("open");
+		/*
+		var windowH = $(window).height();
+		$("nav").css({'height':($(window).height())+'px'});
+		if($("nav").hasClass( "open" )) $("nav").removeClass("open");
+		else $("nav").addClass("open");
+		*/
+	});
+	$('#scrollup').click(function(){$("html, body").animate({ scrollTop: 0 }, 1000);});
+});
